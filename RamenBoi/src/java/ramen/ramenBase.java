35e9e0bd49a0d00ea4d160daf72ca5c650a5ebab
Rafael James Ompad <rafaeljamesompad@gmail.com>
2018-11-24 05:37:24 +0800

    ramenbase Xd

---
+++ RamenBoi/src/java/ramen/ramenBase.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramen;
+
+/**
+ *
+ * @author Rafael
+ */
+public class ramenBase {
+    private float price;
+    private String desc;
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
+    
+}
