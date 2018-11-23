13520aab2975ce2aa4dd37d892c4ae8fb8ad7ed9
bryle99 <brylebaran@gmail.com>
2018-11-24 04:40:05 +0800

    Merge origin/master
    Conflicts:
    	RamenBoi/web/assets/css/style.css

---
+++ RamenBoi/src/java/ramen/miso.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramen;
+
+/**
+ *
+ * @author bryle
+ */
+public class miso extends ramenBase{
+    
+    public miso(){
+        this.setPrice(5);
+    }
+}
