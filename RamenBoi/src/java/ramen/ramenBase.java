5d5e12e1a2bc87721125d7a99dfd432900cd3aa2
bryle99 <brylebaran@gmail.com>
2018-12-06 11:17:44 +0800

    ramen preference

--- RamenBoi/src/java/ramen/ramenBase.java
+++ RamenBoi/src/java/ramen/ramenBase.java
@@ -9,10 +9,49 @@ package ramen;
  *
  * @author Rafael
  */
+
+enum Richness{ 
+        Light, Medium, Rich, VeryRich; 
+} 
+
+enum Firmness{
+        ExtraSoft, Soft, Medium, Firm, ExtraFirm;
+}
+
 abstract class ramenBase {
+    
+    
     
     float price;
     String description;
+    int garlic;
+    boolean scallions;
+    Richness richness;
+    Firmness firmness;
+
+    public boolean isScallions() {
+        return scallions;
+    }
+
+    public void setScallions(boolean scallions) {
+        this.scallions = scallions;
+    }
+
+    public Richness getRichness() {
+        return richness;
+    }
+
+    public void setRichness(Richness richness) {
+        this.richness = richness;
+    }
+
+    public Firmness getFirmness() {
+        return firmness;
+    }
+
+    public void setFirmness(Firmness firmness) {
+        this.firmness = firmness;
+    }
 
     public float getPrice() {
         return price;
